ec4018d04769cf59ff9bb3a3849b5d849bc201c7
michiwo <54099086+michiwo1@users.noreply.github.com>
2021-03-26 14:44:22 +0900

    fea: 完了ボタンを押すと完了したリストに移動する機能を作成

--- src/index.js
+++ src/index.js
@@ -19,6 +19,30 @@ const onClick = () => {
   completeButton.addEventListener("click", () => {
     // 押された削除ボタンの親タグ(div)を未完了リストから削除
     deleteFromIncoimpleteList(completeButton.parentNode);
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    // TODO内容テキストを表示
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグ生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // buttonタグ作成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(削除)タグ生成
